fce5a0931058b5c0d830b6ce00fd80e35c484720
BhushanPatil-technology <bj2077593@gmail.com>
2025-03-30 08:57:33 +0530

    Enabled CORS for all origins

--- src/main/java/com/titto/gaming/titto_gamin_backend/controller/ChallengeController.java
+++ src/main/java/com/titto/gaming/titto_gamin_backend/controller/ChallengeController.java
@@ -1,14 +1,15 @@
 package com.titto.gaming.titto_gamin_backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
+@CrossOrigin(origins = "*") // Allow all origins (temporary solution)
 @RestController
 @RequestMapping("/api")
-//@CrossOrigin("*")
 public class ChallengeController {
     @GetMapping("/welcome")
     public Map<String, Object> getWelcomeContent() {
